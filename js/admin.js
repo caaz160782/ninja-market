5d9bd2acca8e13d0eda30b94c8d2e35238ae40d1
Carlos Aguirre <caaz160782@gmail.com>
2021-10-01 21:17:37 -0500

    cambios minimos

--- js/admin.js
+++ js/admin.js
@@ -70,9 +70,10 @@ document.getElementById("btnSubmit").addEventListener("click", (event)=> {
        //agregar size a new object    
     })    
     //console.log(objectSize)
-    newProduct.sizes=objectSize
-     console.log(newProduct)
-    if(!newProduct) return alert("Camplos obligatorios..")
+    //newProduct.sizes=objectSize
+    newProduct={...newProduct,sizes:objectSize}
+  //   console.log(newProduct)
+    if(!newProduct) return alert("Campos obligatorios..")
     createProduct(newProduct)
 })
 
